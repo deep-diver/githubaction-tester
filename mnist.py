b58e7a13c4a71c60036ece3512fc0fd13cc48762
Chansung Park <deep.diver.csp@gmail.com>
2020-10-06 20:59:07 +0900

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -1,11 +1,9 @@
 from fastai.vision.all import *
 
-path = untar_data(URLs.PETS)
-block = DataBlock(blocks=(ImageBlock, CategoryBlock),
-                  get_items=get_image_files,
-                  get_y=parent_label,
+mnist = DataBlock(blocks=(ImageBlock(cls=PILImageBW), CategoryBlock), 
+                  get_items=get_image_files, 
                   splitter=GrandparentSplitter(),
-                  item_tfms=Resize(224))
-dls = block.dataloaders(path)
-dls.show_batch()
+                  get_y=parent_label)
+dls = mnist.dataloaders(untar_data(URLs.MNIST_TINY))
+dls.show_batch(max_n=9, figsize=(4,4))
 savefig('show_batch.png')
