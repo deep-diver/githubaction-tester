0ba40b6adeaaa23c54e070c2e39ea2aec5640687
Chansung Park <deep.diver.csp@gmail.com>
2020-10-06 15:40:02 +0900

    Create mnist.py

---
+++ mnist.py
@@ -0,0 +1,11 @@
+from fastai.vision.all import *
+
+path = untar_data(URLs.MNIST)
+block = DataBlock(blocks=(ImageBlock(cls=PILImageBW), CategoryBlock),
+                  get_items=get_image_files,
+                  get_y=parent_label,
+                  splitter=GrandparentSplitter(),
+                  item_tfms=Resize(224))
+dls = block.dataloaders(path)
+dls.show_batch()
+savefig('show_batch.png')
