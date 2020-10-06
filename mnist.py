b2cf57307089c61718189e3278320b0a05e4fede
Chansung Park <deep.diver.csp@gmail.com>
2020-10-06 21:06:59 +0900

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -5,6 +5,8 @@ mnist = DataBlock(blocks=(ImageBlock(cls=PILImageBW), CategoryBlock),
                   get_items=get_image_files, 
                   splitter=GrandparentSplitter(),
                   get_y=parent_label)
+
 dls = mnist.dataloaders(untar_data(URLs.MNIST_TINY))
 dls.show_batch(max_n=9, figsize=(4,4))
-matplotlib.pyplot.savefig(('show_batch.png')
+
+matplotlib.pyplot.savefig('show_batch.png')
