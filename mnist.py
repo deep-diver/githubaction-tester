f833b1e0d2af7bbf2322fa882159833f227610c0
Chansung Park <deep.diver.csp@gmail.com>
2020-10-07 00:18:48 +0900

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -1,12 +1,14 @@
 from fastai.vision.all import *
 import matplotlib 
 
+path = untar_data(URLs.MNIST)
 mnist = DataBlock(blocks=(ImageBlock(cls=PILImageBW), CategoryBlock), 
                   get_items=get_image_files, 
-                  splitter=GrandparentSplitter(),
-                  get_y=parent_label)
+                  splitter=GrandparentSplitter(train_name='training', valid_name='testing'),
+                  get_y=parent_label,
+                  item_tfms=Resize(224))
 
-dls = mnist.dataloaders(untar_data(URLs.MNIST_TINY))
+dls = mnist.dataloaders(path)
 dls.show_batch(max_n=9, figsize=(4,4))
 
 matplotlib.pyplot.savefig('show_batch.png')
