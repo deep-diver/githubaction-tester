8d7ef16006881ede16c97535909f4e14cdc1eb54
Chansung Park <deep.diver.csp@gmail.com>
2020-10-07 09:01:30 +0900

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -9,6 +9,17 @@ mnist = DataBlock(blocks=(ImageBlock(cls=PILImageBW), CategoryBlock),
                   item_tfms=Resize(224))
 
 dls = mnist.dataloaders(path)
-dls.show_batch(max_n=9, figsize=(4,4))
+dls.show_batch(max_n=9, figsize=(5,5))
 
 matplotlib.pyplot.savefig('show_batch.png')
+
+learn = cnn_learner(dls, resnet34, metrics=accuracy)
+learn.fine_tune(3)
+learn.recorder.plot_loss()
+
+matplotlib.pyplot.savefig('loss.png')
+
+interp = ClassificationInterpretation.from_learner(learn)
+interp.plot_confusion_matrix(figsize=(5,5))
+
+matplotlib.pyplot.savefig('confusion_matrix.png')
