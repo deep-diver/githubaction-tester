1facf0dbc5660b7ceb392a5c9faeb585187409e1
Chansung Park <deep.diver.csp@gmail.com>
2020-10-07 10:15:51 +0900

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -10,7 +10,7 @@ mnist = DataBlock(blocks=(ImageBlock(cls=PILImageBW), CategoryBlock),
                   get_y=parent_label)
 
 dls = mnist.dataloaders(path)
-dls.show_batch(max_n=9, figsize=(5,5))
+dls.show_batch(max_n=9)
 
 matplotlib.pyplot.savefig('show_batch.png')
 matplotlib.pyplot.clf()
@@ -23,7 +23,7 @@ matplotlib.pyplot.savefig('loss.png')
 matplotlib.pyplot.clf()
 
 interp = ClassificationInterpretation.from_learner(learn)
-interp.plot_confusion_matrix(figsize=(5,5))
+interp.plot_confusion_matrix()
 
 matplotlib.pyplot.savefig('confusion_matrix.png')
 matplotlib.pyplot.clf()
