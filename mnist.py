5a543fcc8e533d6360e44577cd2f06d73075182d
Chansung Park <deep.diver.csp@gmail.com>
2020-10-07 10:23:20 +0900

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -1,8 +1,6 @@
 from fastai.vision.all import *
 import matplotlib 
 
-matplotlib.rcParams['figure.figsize'] = (3.0, 3.0)
-
 path = untar_data(URLs.MNIST_TINY)
 mnist = DataBlock(blocks=(ImageBlock(cls=PILImageBW), CategoryBlock), 
                   get_items=get_image_files, 
@@ -10,13 +8,14 @@ mnist = DataBlock(blocks=(ImageBlock(cls=PILImageBW), CategoryBlock),
                   get_y=parent_label)
 
 dls = mnist.dataloaders(path)
-dls.show_batch(max_n=5, ncols=5, nrows=1, figsize=(5,5))
+dls.show_batch(max_n=5, ncols=5, nrows=1, figsize=(6,2))
 
 matplotlib.pyplot.savefig('show_batch.png')
 matplotlib.pyplot.clf()
 
 learn = cnn_learner(dls, resnet34, metrics=accuracy)
 learn.fine_tune(3)
+matplotlib.pyplot.figure(figsize=(6,3))
 learn.recorder.plot_loss()
 
 matplotlib.pyplot.savefig('loss.png')
