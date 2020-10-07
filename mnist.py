800d8d02ba322c8eaeb161d38148448edfbdcae9
chansung <chansung@chansungs-iMac.local>
2020-10-07 15:26:55 +0900

    new mnist.py

--- mnist.py
+++ mnist.py
@@ -1,28 +1,43 @@
 from fastai.vision.all import *
 import matplotlib 
 
-path = untar_data(URLs.MNIST_TINY)
-mnist = DataBlock(blocks=(ImageBlock(cls=PILImageBW), CategoryBlock), 
-                  get_items=get_image_files, 
-                  splitter=GrandparentSplitter(),
-                  get_y=parent_label)
+train = pd.read_csv(f'data/train.csv')
 
-dls = mnist.dataloaders(path)
+def get_items(path):
+    train_df = pd.read_csv(f'{PATH}/train.csv')
+    return train_df.to_numpy().tolist()
+
+def get_x(item):
+    if len(item) == 785:
+        x = np.array(item[1:]).reshape(28, 28).astype(np.uint8)
+    else:
+        x = np.array(item).reshape(28, 28).astype(np.uint8)
+    return x
+
+def get_y(item):
+    return item[0]
+
+db = DataBlock(blocks=(ImageBlock(cls=PILImageBW), CategoryBlock),
+          get_items=get_items,
+          get_x=get_x,
+          splitter=RandomSplitter(seed=42),
+          get_y=get_y)
+
+dls = DataLoaders.from_dblock(db, df=train, source=PATH, bs=128)
 dls.show_batch(max_n=5, ncols=5, nrows=1, figsize=(6,2))
 
 matplotlib.pyplot.savefig('show_batch.png')
 matplotlib.pyplot.clf()
 
-learn = cnn_learner(dls, resnet34, metrics=accuracy)
-learn.fine_tune(3)
+learn = cnn_learner(dls, resnet34, metrics=[accuracy, error_rate])
+learn.fine_tune(5)
+
 matplotlib.pyplot.figure(figsize=(6,3))
 learn.recorder.plot_loss()
-
 matplotlib.pyplot.savefig('loss.png')
 matplotlib.pyplot.clf()
 
 interp = ClassificationInterpretation.from_learner(learn)
-interp.plot_confusion_matrix(figsize=(3,3))
-
+interp.plot_confusion_matrix(figsize=(5,5))
 matplotlib.pyplot.savefig('confusion_matrix.png')
 matplotlib.pyplot.clf()
