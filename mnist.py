723995b1ff9323e5a19abe6d8fc354cca77925b3
Chansung Park <deep.diver.csp@gmail.com>
2020-10-07 10:11:40 +0900

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -11,14 +11,17 @@ dls = mnist.dataloaders(path)
 dls.show_batch(max_n=9, figsize=(5,5))
 
 matplotlib.pyplot.savefig('show_batch.png')
+matplotlib.pyplot.clf()
 
 learn = cnn_learner(dls, resnet34, metrics=accuracy)
 learn.fine_tune(3)
 learn.recorder.plot_loss()
 
 matplotlib.pyplot.savefig('loss.png')
+matplotlib.pyplot.clf()
 
 interp = ClassificationInterpretation.from_learner(learn)
 interp.plot_confusion_matrix(figsize=(5,5))
 
 matplotlib.pyplot.savefig('confusion_matrix.png')
+matplotlib.pyplot.clf()
